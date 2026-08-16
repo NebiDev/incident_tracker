ffe8ae7c08fcd4bd1b55cf56e7a553d67241af9d
Bilisuma Nebi <191428554+NebiDev@users.noreply.github.com>
2026-08-16 16:18:25 -0500

    styled the active link of navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,12 +1,19 @@
+"use client"
+
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { Bug } from 'lucide-react';
+import {clsx} from 'clsx'
+import { link } from 'fs/promises';
 
 const Navbar = () => {
+  const pathname = usePathname()
+
 
   const links = [
     { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues/list" },
+    { label: "Issues", href: "/issues" },
   ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
@@ -14,7 +21,7 @@ const Navbar = () => {
       <ul className="flex space-x-6 ">
         {links.map((link) => (
           <li key={link.href}>
-            <Link className="text-zinc-500 hover:text-zinc-800 transition-colors" href={link.href}>
+            <Link href={link.href} className={clsx("hover:text-blue-500", pathname === link.href && "text-blue-500")}>
               {link.label}
             </Link>
           </li>
---
+++ app/issues/page.tsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+
+
+const IssuesPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default IssuesPage
