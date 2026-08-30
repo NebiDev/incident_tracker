c484dc01d69b7a4fc7b2d089ed5df52c0c7203dc
Bilisuma Nebi <191428554+NebiDev@users.noreply.github.com>
2026-08-30 09:37:33 -0500

    refactor the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -5,72 +5,106 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { Bug } from 'lucide-react';
 import { clsx } from 'clsx'
-import { Box, Flex, Container, DropdownMenu, Avatar, Text } from '@radix-ui/themes'
-import { useSession } from 'next-auth/react'
+import { Box, Flex,  DropdownMenu, Avatar, Text } from '@radix-ui/themes'
+import { useSession, signOut } from 'next-auth/react'
 
 
 const Navbar = () => {
-    const pathname = usePathname()
-    const { status, data: session } = useSession()
 
-
-    const links = [
-        { label: "Dashboard", href: "/" },
-        { label: "Incidents", href: "/incidents" },
-    ];
     return (
         <nav className=" border-b mb-4 px-5 py-3 ">
-           
+
             <Flex justify="between" >
-                <Flex align="center" gap='3'><Link href="/"><Bug /></Link>
-                    <ul className="flex space-x-6 ">
-                        {links.map((link) => (
-                            <li key={link.href}>
-                                <Link href={link.href} className={clsx("hover:text-blue-500", pathname === link.href && "text-blue-500")}>
-                                    {link.label}
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
+                <Flex align="center" gap='3'>
+                    <Link href="/"><Bug size={20} /></Link>
+                    {/* <Link href="/" className="flex items-center gap-2 font-semibold">
+                        <Bug size={20} />
+                        <span>Incident Tracker</span>
+                    </Link> */}
+                    <NavLinks/>
+
+
                 </Flex>
-                <Box>
-                    {status === "authenticated" && (
-                        <DropdownMenu.Root>
-                            <DropdownMenu.Trigger >
-                                <Avatar 
-                                src={session.user?.image!} 
-                                fallback={session.user?.name?.[0] ?? "U"}
-                                size="2"
-                                radius="full" 
-                                className="cursor-pointer"
-                                />
-                                {/* <button className="hover:text-blue-500">{session.user?.name}</button> */}
-                            </DropdownMenu.Trigger>
-                            <DropdownMenu.Content>
-                                <DropdownMenu.Label>
-                                    <Text size="2" weight="medium">
-                                        {session.user?.name}
-                                    </Text>
-                                </DropdownMenu.Label>
-                                <DropdownMenu.Item>
-                                    <Link href="/api/auth/signout" >Sign Out</Link>
-                                </DropdownMenu.Item>
+                <AuthStatus />
 
-                            </DropdownMenu.Content> 
 
-                        </DropdownMenu.Root>
-                        // <Link href="/api/auth/signout" className="hover:text-blue-500">Sign Out</Link>
-                    )}
-                    {status === "unauthenticated" && (
-                        <Link href="/api/auth/signin" className="hover:text-blue-500">Sign In</Link>
-                    )}
-                </Box>
             </Flex>
-            
+
 
 
 
         </nav>
+    )
+}
+
+const NavLinks = () => {
+    const pathname = usePathname()
+    const links = [
+        { label: "Dashboard", href: "/" },
+        { label: "Incidents", href: "/incidents" },
+    ];
+
+
+    return (
+        <ul className="flex space-x-6 ">
+            {links.map((link) => (
+                <li key={link.href}>
+                    <Link 
+                        href={link.href}
+                        className={clsx({
+                            "nav-link": true,
+                            "text-blue-500": link.href === pathname,
+                        })}
+                        
+                    >
+                        {link.label}
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+const AuthStatus = () => {
+    const { status, data: session } = useSession()
+
+    if (status === "loading") return null;
+    if (status === "unauthenticated") 
+    return <Link href="/api/auth/signin" className="hover:text-blue-500">Sign In</Link>
+
+
+
+    return (
+        <Box>
+            
+                <DropdownMenu.Root>
+                    <DropdownMenu.Trigger >
+                        <Avatar
+                            src={session!.user!.image!}
+                            fallback="?"
+                            size="2"
+                            radius="full"
+                            className="cursor-pointer"
+                        />
+                    </DropdownMenu.Trigger>
+                    <DropdownMenu.Content>
+                        <DropdownMenu.Label>
+                            <Text size="2" weight="medium">
+                                {session!.user?.name}
+                            </Text>
+                        </DropdownMenu.Label>
+                    <DropdownMenu.Item onClick={() => signOut()}>
+                            {/* <Link href="/api/auth/signout" >Sign Out</Link> */}
+                        Sign Out
+                        </DropdownMenu.Item>
+
+
+                    </DropdownMenu.Content>
+
+                </DropdownMenu.Root>
+       
+            
+        </Box>
     )
 }
 
--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,17 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'referrer-policy', value: 'no-referrer' },
+          
+        ],
+      },
+    ];
+  }
 };
 
 export default nextConfig;
